e35019492055c086706489f3082a99afd1c754c8
nateachino <natealleyne88@gmail.com>
2024-10-21 23:37:00 -0400

    Added functionality for creating redis instances

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -3,14 +3,12 @@ package main
 import (
 	"context"
 	"fmt"
-	"io"
-	"log"
 	"net"
-	"os"
+	"strconv"
+	"strings"
 	"time"
 
 	"github.com/docker/docker/api/types/container"
-	"github.com/docker/docker/api/types/filters"
 	"github.com/docker/docker/api/types/image"
 	"github.com/docker/docker/client"
 	"github.com/docker/go-connections/nat"
@@ -19,50 +17,8 @@ import (
 
 var ctx = context.Background()
 
-func startRedis(cli *client.Client) {
-	fmt.Println("Starting Redis via Docker")
-
-	reader, err := cli.ImagePull(ctx, "docker.io/library/redis", image.PullOptions{})
-
-	if err != nil{
-		panic(err)
-	}
-
-	defer reader.Close()
-
-	io.Copy(os.Stdout, reader)
-
-	resp, err := cli.ContainerCreate(ctx, &container.Config{
-		Image: "redis",
-		Cmd: []string{},
-		Tty: false,
-
-	}, &container.HostConfig{
-		PortBindings: map[nat.Port][]nat.PortBinding{
-			"6379/tcp": {
-				{
-					HostIP:   "0.0.0.0",
-					HostPort: "6379",
-				},
-			},
-		},
-	}, nil, nil, "")
-
-	if err != nil{
-		panic(err)
-	}
-
-	
-	if err := cli.ContainerStart(ctx, resp.ID, container.StartOptions{}); err != nil{
-		panic(err)
-	}
-	
-	if err := waitForRedis("localhost", "6379", 30*time.Second); err != nil {
-		log.Fatalf("Redis did not start within the expected time: %v", err)
-	}
-}	
-
 func main(){
+	//TODO: Connect to existing redis instance if open
 	cli, err := client.NewClientWithOpts(client.FromEnv, client.WithAPIVersionNegotiation())
 
 	if err != nil{
@@ -71,35 +27,81 @@ func main(){
 
 	defer cli.Close()
 
-	filterArgs := filters.NewArgs()
-	filterArgs.Add("ancestor", "redis")
+	port := 6379
 
-	containers, err := cli.ContainerList(ctx, container.ListOptions{Filters: filterArgs})
+	for isPortTaken(port){
+		port++
+	}
+
+	startRedis(cli, port)
+
+	hostAddress := fmt.Sprintf("localhost:%d", port)
+
+	rdb := redis.NewClient(&redis.Options{
+		Addr: hostAddress,
+		Password: "",
+		DB: 0,
+	})
 
 	if err != nil{
 		panic(err)
 	}
 
-	if len(containers) == 0 {
-		defer startRedis(cli) 
+	if err := waitForRedis("localhost", string(port), 30*time.Second, rdb); err != nil {
+		panic("Redis did not start within the expected time")
 	}
-
-	rdb := redis.NewClient(&redis.Options{
-		Addr: "localhost:6379",
-		Password: "",
-		DB: 0,
-	})
-
-	pong, err := rdb.Ping(ctx).Result()
-
-	if err != nil{
-		panic("Couldn't connect to Redis")
-	}
-
-	fmt.Println("Connected to Redis:", pong)
+	
+	fmt.Println("Connected to Redis")
 }
 
-func waitForRedis(host, port string, timeout time.Duration) error {
+func startRedis(cli *client.Client, port int) bool {
+	//TODO: Add DB Username and Password with cat'ed redis config file
+	//TODO: Check if port is used prior to creating a new container
+	fmt.Println("Starting Redis via Docker")
+
+	reader, err := cli.ImagePull(ctx, "docker.io/library/redis", image.PullOptions{})
+	if err != nil {
+		panic(err)
+	}
+	defer reader.Close()
+
+	var binding nat.Port = nat.Port(fmt.Sprintf("%d/tcp", port))
+
+	resp, err := cli.ContainerCreate(ctx, &container.Config{
+		Image: "redis",
+		Cmd:   []string{"redis-server", "--port", strconv.Itoa(port)},
+		Tty:   false,
+		ExposedPorts: nat.PortSet{
+			binding: struct{}{},
+		},
+	}, &container.HostConfig{
+		PortBindings: nat.PortMap{
+			binding: []nat.PortBinding{
+				{
+					HostIP:   "0.0.0.0",
+					HostPort: strconv.Itoa(port),
+				},
+			},
+		},
+	}, nil, nil, "")
+
+	if err != nil {
+		panic(err)
+	}
+
+	if err := cli.ContainerStart(ctx, resp.ID, container.StartOptions{}); err != nil {
+		if strings.Contains(err.Error(), "port is already allocated") {
+			fmt.Printf("Port %d is already allocated, trying port %d\n", port, port+1)
+			return false
+		} else {
+			panic(err)
+		}
+	}
+	return true
+}
+
+func waitForRedis(host, port string, timeout time.Duration, redisClient *redis.Client) error {
+	//TODO: Figure out why it wont connect to freshly created containers
 	address := net.JoinHostPort(host, port)
 	deadline := time.Now().Add(timeout)
 
@@ -108,11 +110,12 @@ func waitForRedis(host, port string, timeout time.Duration) error {
 	delay := initialDelay
 
 	for time.Now().Before(deadline) {
-		conn, err := net.DialTimeout("tcp", address, 1*time.Second)
-		if err == nil {
-			conn.Close()
-			return nil 
+		_, err := redisClient.Ping(ctx).Result()
+		
+		if err == nil{
+			return nil
 		}
+		
 		fmt.Printf("Waiting for Redis to be ready, retrying in %v...\n", delay)
 
 		time.Sleep(delay)
@@ -124,4 +127,16 @@ func waitForRedis(host, port string, timeout time.Duration) error {
 	}
 
 	return fmt.Errorf("timeout: Redis did not become available on %s within %v", address, timeout)
+}
+
+func isPortTaken(port int) bool{
+	address := fmt.Sprintf(":%d", port)
+
+	listener, err := net.Listen("tcp", address)
+	if err != nil{
+		fmt.Printf("Port %d is in use. Attempting to connect to port %d\n", port, port+1)
+		return true
+	}
+	listener.Close()
+	return false
 }
